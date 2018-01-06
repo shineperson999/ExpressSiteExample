c481a1ca5a65960ecfe4c80952da660416cbebf3
shineperson999 <35166827+shineperson999@users.noreply.github.com>
2018-01-06 19:42:24 +0800

    Update connectivity.js

--- scripts/connectivity.js
+++ scripts/connectivity.js
@@ -4,6 +4,9 @@
  * 
  */
 
+const TIMEOUT = 1500;
+let c = 0;
+
 function ping(ip, callback) {
 
     if (!this.inUse) {
@@ -13,15 +16,15 @@ function ping(ip, callback) {
         this.ip = ip;
         var _that = this;
         this.img = new Image();
-        this.img.onload = function () {
+        this.img.onload = function (e) {
             _that.inUse = false;
-            _that.callback('ONLINE');
+            _that.callback('responded', e);
 
         };
         this.img.onerror = function (e) {
             if (_that.inUse) {
                 _that.inUse = false;
-                _that.callback('ONLINE', e);
+                _that.callback('responded', e);
             }
 
         };
@@ -32,7 +35,7 @@ function ping(ip, callback) {
                 _that.inUse = false;
                 _that.callback('timeout');
             }
-        }, 1500);
+        }, TIMEOUT);
     }
 }
 var PingModel = function (servers) {
@@ -41,15 +44,34 @@ var PingModel = function (servers) {
     ko.utils.arrayForEach(servers, function (location) {
         myServers.push({
             name: location,
-            status: ko.observable('PENDING')
+            status: ko.observable('unchecked'),
+            retry:  ko.observable(0)
         });
     });
     self.servers = ko.observableArray(myServers);
     ko.utils.arrayForEach(self.servers(), function (s) {
-        s.status('CONTACTING');
-        new ping(s.name, function (status, e) {
-            s.status(status);
-        });
+        s.status('checking');
+        alertWhenLive(s, function (status, e) {console.log(s.name,'callback retries', s.retry)
+            s.status(status);debugger;
+            s.retry(s.retry() + 1);
+        }, function (name) { alert(name + " is back online!") });
+        
+        function alertWhenLive (server, callback, alertCallback) {
+        	
+          pingUntilSuccess();
+          
+          function pingUntilSuccess (status, e) {
+            if (status) callback(status, e);
+            console.log(server.name, 'status')
+						if (status && status !== 'timeout') {debugger;
+            	if (server.retry() > 1) alertCallback(server.name);
+              
+              return;
+            }
+            
+            new ping(server.name, pingUntilSuccess);
+        	}
+        }
     });
 };
 var komodel = new PingModel([
